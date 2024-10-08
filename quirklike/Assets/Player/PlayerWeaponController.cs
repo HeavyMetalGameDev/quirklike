dbdd45b3316e01158d3fbd811fffd778245289b7
HeavyMetalGameDev <126513139+HeavyMetalGameDev@users.noreply.github.com>
2024-10-08 22:57:11 +0100

    Reworked PlayerWeaponController
    - Reworked PlayerWeaponController to have weapon slots that weapons can be assigned to. This will reparent a weapon to the slot. More should be done with this e.g disabling colliders and rigidbodies and the like.
    - Added WeaponSlot class which can hold a transform and a WeaponBase.

--- quirklike/Assets/Player/PlayerWeaponController.cs
+++ quirklike/Assets/Player/PlayerWeaponController.cs
@@ -2,11 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class WeaponSlot
+{
+    public WeaponBase weapon;
+    public Transform transform;
+
+    public void ReparentWeapon()
+    {
+        weapon.transform.parent = transform;
+        weapon.transform.localPosition = Vector3.zero;
+        weapon.transform.localScale = Vector3.one;
+        weapon.transform.localRotation = Quaternion.identity;
+    }
+
+    public void DropWeaponFromSlot()
+    {
+        weapon.transform.parent = null;
+        //maybe move it to a different place / give it some velocity? / reenable rigidbody
+    }
+}
+
 public class PlayerWeaponController : MonoBehaviour
 {
-    [SerializeField] WeaponBase testWeapon;
-
-    List<WeaponBase> currentWeapons;
+    [SerializeField] List<WeaponSlot> currentWeaponSlots;
+    [SerializeField] List<WeaponBase> testWeapons;
+    int numberOfAssignedWeapons = 0; 
 
     public event System.Action OnPlayerFireClicked;
     public event System.Action OnPlayerFireHeld;
@@ -15,14 +36,16 @@ public class PlayerWeaponController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentWeapons = new List<WeaponBase>();
-        AttachWeapon(testWeapon);
+        foreach(WeaponBase weapon in testWeapons) //this is only here for testing.
+        {
+            AttachWeapon(weapon);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //change these to new input system
         {
             OnPlayerFireClicked?.Invoke();
         }
@@ -36,10 +59,24 @@ public class PlayerWeaponController : MonoBehaviour
         }
     }
 
+    void AttachAlreadyHeldWeapon(WeaponBase weapon) //used when the weapon is already in the current weapons list, might be needed sometimes idk
+    {
+        Debug.Log(weapon.gameObject);
+        OnPlayerFireClicked += weapon.OnInputClicked;
+        OnPlayerFireHeld += weapon.OnInputHeld;
+        OnPlayerFireReleased += weapon.OnInputReleased;
+    }
+
     void AttachWeapon(WeaponBase weapon)
     {
         Debug.Log(weapon.gameObject);
-        currentWeapons.Add(weapon);
+
+        WeaponSlot freeSlot = GetFreeWeaponSlot();
+        if (freeSlot == null) return; //all slots are ful :(
+        freeSlot.weapon = weapon;
+        freeSlot.ReparentWeapon();
+
+        numberOfAssignedWeapons++;
 
         OnPlayerFireClicked += weapon.OnInputClicked;
         OnPlayerFireHeld += weapon.OnInputHeld;
@@ -48,11 +85,25 @@ public class PlayerWeaponController : MonoBehaviour
 
     void DropWeapon(int weaponIndexID)
     {
-        WeaponBase droppedWeapon = currentWeapons[weaponIndexID];
-        currentWeapons.Remove(droppedWeapon);
+        WeaponSlot droppedSlot = currentWeaponSlots[weaponIndexID];
+        WeaponBase droppedWeapon = droppedSlot.weapon;
+        droppedSlot.weapon = null;
 
+        numberOfAssignedWeapons--;
         OnPlayerFireClicked -= droppedWeapon.OnInputClicked;
         OnPlayerFireHeld -= droppedWeapon.OnInputHeld;
         OnPlayerFireReleased -= droppedWeapon.OnInputReleased;
+    }
+
+    WeaponSlot GetFreeWeaponSlot()
+    {
+        foreach(WeaponSlot slot in currentWeaponSlots)
+        {
+            if (slot.weapon == null)
+            {
+                return slot;
+            }
+        }
+        return null;
     }
 }
