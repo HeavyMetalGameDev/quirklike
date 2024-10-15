70199f73d2b97c6b7fe3ff368acfe9dcbc10a950
Eddu <55552484+St4rman@users.noreply.github.com>
2024-10-15 14:19:44 +0100

    Added a Patrolling state
    * Now the wanderer script has an array of Vec3s that will be treated as patrol points.
    * There is a known bug where soemtimes when switching off the nav agent the enemy falls through the floor but that can be fixes with proper hit box alignment so we should be good when we get an actual model

---
+++ quirklike/Assets/AI/FSM/States/Wanderer Specific/PatrolState.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class PatrolState : State
+{
+    private readonly Wanderer wanderer;
+    private readonly NavMeshAgent NavAgent;
+
+    private readonly Vector3[] PatrolPoints;
+    int CurPoint      = 0;
+    bool TargetSwitch = false;
+
+    float WaitTime = 0.1f;
+
+    //private readonly animator etc etc
+
+    public PatrolState(Wanderer w, NavMeshAgent a, Vector3[] patrolpoints)
+    {
+        wanderer = w;
+        NavAgent = a;
+        PatrolPoints = patrolpoints;
+    }
+
+    public void Tick()
+    {
+        if(!NavAgent.pathPending)
+        {
+            if(NavAgent.remainingDistance < NavAgent.stoppingDistance)
+            {
+                if(!NavAgent.hasPath || NavAgent.velocity.sqrMagnitude == 0f)
+                {
+                    if(!TargetSwitch)
+                    {
+                       SwitchTargets();
+                    } 
+                }
+                
+            }
+        }
+        
+    }
+
+    void SwitchTargets()
+    {
+        TargetSwitch = true;
+        CurPoint += 1;
+        if(CurPoint >= PatrolPoints.Length)
+        {
+            CurPoint =0;
+        }
+        NavAgent.SetDestination(PatrolPoints[CurPoint]);
+        TargetSwitch = false;
+    }
+
+    public void OnEnter()
+    {
+        NavAgent.enabled = true;
+        NavAgent.SetDestination(PatrolPoints[0]);
+    }
+
+    public void OnExit()
+    {
+        NavAgent.enabled = false;
+        TargetSwitch     = false;
+        CurPoint = 0;
+    }
+}
