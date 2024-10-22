8287b8c0c664962bb5d67e48ab3b81c8fb479a29
HeavyMetalGameDev <126513139+HeavyMetalGameDev@users.noreply.github.com>
2024-10-22 17:52:21 +0100

    Added various Callback events
    - Added various events to the Callbacks class. These may need to be altered in the future but I think they should all be used at some point.

--- quirklike/Assets/General Scripts/Callbacks.cs
+++ quirklike/Assets/General Scripts/Callbacks.cs
@@ -11,13 +11,38 @@ public enum CallbackEvent
 {
     NullEvent,
     TestEvent,
-    TestIntEvent
+    TestIntEvent,
+    EnemyKilled,
+    EnemySpawned,
+    SceneLoaded,
+    RoomEntered,
+    WaveCompleted,
+    RoomCompleted,
+    WeaponPickedUp,
+    WeaponDropped,
+    PlayerHurt,
+    PlayerKilled,
+    GameLost,
+    AreaComplete
 }
 
 
 public static class Callbacks
 {
+    // these events may need their own parameters in the future
     public static event System.Action TestEvent;
+    public static event System.Action EnemyKilled;
+    public static event System.Action EnemySpawned;
+    public static event System.Action SceneLoaded;
+    public static event System.Action RoomEntered;
+    public static event System.Action WaveCompleted;
+    public static event System.Action RoomCompleted;
+    public static event System.Action WeaponPickedUp;
+    public static event System.Action WeaponDropped;
+    public static event System.Action PlayerHurt;
+    public static event System.Action PlayerKilled;
+    public static event System.Action GameLost;
+    public static event System.Action AreaComplete;
 
 
     public static void CallEvent(CallbackEvent callbackEvent, CallbackData data = null) //this can be called from anywhere, be careful
@@ -28,6 +53,84 @@ public static class Callbacks
                 {
                     Debug.Log("TEST EVENT CALLED");
                     TestEvent?.Invoke();
+                    break;
+                }
+            case CallbackEvent.EnemyKilled:
+                {
+                    Debug.Log("ENEMY HAS BEEN KILLED");
+                    EnemyKilled?.Invoke();
+                    break;
+                }
+            case CallbackEvent.EnemySpawned:
+                {
+                    Debug.Log("ENEMY SPAWNED");
+                    EnemySpawned?.Invoke();
+                    break;
+                }
+            case CallbackEvent.SceneLoaded:
+                {
+                    Debug.Log("SCENE LOADED");
+                    SceneLoaded?.Invoke();
+                    break;
+                }
+            case CallbackEvent.RoomEntered:
+                {
+                    Debug.Log("ROOM ENTERED");
+                    RoomEntered?.Invoke();
+                    break;
+                }
+            case CallbackEvent.WaveCompleted:
+                {
+                    Debug.Log("WAVE COMPLETED");
+                    WaveCompleted?.Invoke();
+                    break;
+                }
+            case CallbackEvent.RoomCompleted:
+                {
+                    Debug.Log("ROOM COMPLETED");
+                    RoomCompleted?.Invoke();
+                    break;
+                }
+            case CallbackEvent.WeaponPickedUp:
+                {
+                    Debug.Log("WEAPON PICKED UP");
+                    WeaponPickedUp?.Invoke();
+                    break;
+                }
+            case CallbackEvent.WeaponDropped:
+                {
+                    Debug.Log("WEAPON DROPPED");
+                    WeaponDropped?.Invoke();
+                    break;
+                }
+            case CallbackEvent.PlayerHurt:
+                {
+                    Debug.Log("PLAYER HURT");
+                    PlayerHurt?.Invoke();
+                    break;
+                }
+            case CallbackEvent.PlayerKilled:
+                {
+                    Debug.Log("PLAYER KILLED");
+                    PlayerKilled?.Invoke();
+                    break;
+                }
+            case CallbackEvent.GameLost:
+                {
+                    Debug.Log("GAME LOST");
+                    GameLost?.Invoke();
+                    break;
+                }
+            case CallbackEvent.AreaComplete:
+                {
+                    Debug.Log("AREA COMPLETE");
+                    AreaComplete?.Invoke();
+                    break;
+                }
+            default:
+                {
+                    Debug.LogError("EVENT NOT IMPLEMENTED");
+
                     break;
                 }
         }
