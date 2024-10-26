1f69b365e875e49a7bcabe7b8f685848f634dec6
Eddu <55552484+St4rman@users.noreply.github.com>
2024-10-26 19:26:48 +0100

    Reactive Reticle 2

--- quirklike/Assets/UI/Scritps/ReactiveReticle.cs
+++ quirklike/Assets/UI/Scritps/ReactiveReticle.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.UI;
 
 public class ReactiveReticle : MonoBehaviour
 {
     private RectTransform ReticleTransform;
 
-    [Range(50f, 50f)]
+    [Range(10f, 250f)]
     public float size;
+    private float currentSize;
     void Awake()
     {
         ReticleTransform = GetComponent<RectTransform>();    
     }
+
+    public void ReticleBloom(){
+        ReticleTransform.DOSizeDelta(new Vector2(size, size), 2f, false);
+    }
+
     void Update()
     {
-        ReticleTransform.sizeDelta = new Vector2(size, size); 
+        if(Input.GetKeyDown(KeyCode.U)){
+            ReticleBloom();
+        } 
     }
 }
