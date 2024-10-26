ac093cb5dc58a3b09c1bc86d45a3db4a91efcc6c
Eddu <55552484+St4rman@users.noreply.github.com>
2024-10-26 18:15:11 +0100

    DoTween my bleoved

--- quirklike/Assets/UI/Scritps/HealthBarUpdate.cs
+++ quirklike/Assets/UI/Scritps/HealthBarUpdate.cs
@@ -1,32 +1,60 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
 
 public class HealthBarUpdate : MonoBehaviour
 {
     private float MaxHealth = 100;
     private float CurrentHealth;
+
+    [Header("Variables")]
+    [SerializeField] private float FillSpeed;
+    [SerializeField] private Gradient healthGrad;
+    public int CountFps = 30;
+    public float Duration = 1f;
+
+    [Header("References")]
     [SerializeField] private Image HealthBarFill;
-    // Start is called before the first frame update
+    [SerializeField] private TMP_Text HealthTxt; 
+
+   
     void Start()
     {
-        CurrentHealth = 50;
+        UpdateHealthUI(50);
+    }
+
+    public void UpdateHealthUI(float curHP)
+    {
+
+        CurrentHealth = curHP;
+        CurrentHealth = Mathf.Clamp(CurrentHealth, 0f, MaxHealth);
         UpdateHealthBarFill();
+        UpdateHealthTxt();
     }
 
     private void UpdateHealthBarFill(){
+       
         float amt = CurrentHealth / MaxHealth;
-        HealthBarFill.fillAmount = amt;
+        HealthBarFill.DOFillAmount(amt, FillSpeed);
+        HealthBarFill.DOColor(healthGrad.Evaluate(amt), FillSpeed);
+
+    }
+    private void UpdateHealthTxt(){
+        HealthTxt.text = "" +CurrentHealth + "%";
     }
 
     void Update()
     {
         if(Input.GetKeyDown("right")){
             CurrentHealth += 10;
-            UpdateHealthBarFill();
+            UpdateHealthUI(CurrentHealth);
+
         }
         if(Input.GetKeyDown("left")){
-            UpdateHealthBarFill();
-            CurrentHealth -= 10;
+            CurrentHealth -=10;
+            UpdateHealthUI(CurrentHealth);
         }
     }
 }
