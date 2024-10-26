9587f3bf2a02075439f070073fd51d8e7ebfdc1a
Eddu <55552484+St4rman@users.noreply.github.com>
2024-10-26 18:40:45 +0100

    Counting Health

--- quirklike/Assets/UI/Scritps/HealthBarUpdate.cs
+++ quirklike/Assets/UI/Scritps/HealthBarUpdate.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
@@ -13,25 +14,54 @@ public class HealthBarUpdate : MonoBehaviour
     [SerializeField] private float FillSpeed;
     [SerializeField] private Gradient healthGrad;
     public int CountFps = 30;
-    public float Duration = 1f;
+    public float Duration = 10f;
 
     [Header("References")]
     [SerializeField] private Image HealthBarFill;
     [SerializeField] private TMP_Text HealthTxt; 
 
-   
-    void Start()
+    private Coroutine CountRoutine;
+    private int _value;
+
+
+    private void UpdateHText(int NewValue)
     {
-        UpdateHealthUI(50);
+        if (CountRoutine != null) {
+            StopCoroutine(CountRoutine);
+        }
+        CountRoutine = StartCoroutine(CountHealth(NewValue));
     }
 
-    public void UpdateHealthUI(float curHP)
-    {
 
-        CurrentHealth = curHP;
-        CurrentHealth = Mathf.Clamp(CurrentHealth, 0f, MaxHealth);
-        UpdateHealthBarFill();
-        UpdateHealthTxt();
+    private IEnumerator CountHealth(int NewValue)
+    {
+        WaitForSeconds wait = new WaitForSeconds(1/CountFps);
+        int prevVal = _value;
+        int stepAmt;
+
+        if(NewValue - prevVal < 0){
+            stepAmt = Mathf.FloorToInt((NewValue - prevVal) / (CountFps * Duration));
+        }
+        else{
+            stepAmt = Mathf.CeilToInt((NewValue - prevVal) / (CountFps * Duration));
+        }
+
+        if(prevVal < NewValue)
+        {
+            while(prevVal < NewValue)
+            {
+                prevVal += stepAmt;
+                
+                if(prevVal > NewValue)
+                {
+                    prevVal = NewValue;
+                }
+
+                HealthTxt.text = prevVal.ToString("N0");
+
+                yield return wait;
+            }
+        }
     }
 
     private void UpdateHealthBarFill(){
@@ -41,19 +71,30 @@ public class HealthBarUpdate : MonoBehaviour
         HealthBarFill.DOColor(healthGrad.Evaluate(amt), FillSpeed);
 
     }
-    private void UpdateHealthTxt(){
-        HealthTxt.text = "" +CurrentHealth + "%";
+
+    public void UpdateHealthUI(float curHP)
+    {
+
+        CurrentHealth = curHP;
+        UpdateHealthBarFill();
+        UpdateHText((int)CurrentHealth);
     }
 
+
+    void Start()
+    {
+        UpdateHealthUI(CurrentHealth);
+    }
+    //Debug Stuff
     void Update()
     {
         if(Input.GetKeyDown("right")){
-            CurrentHealth += 10;
-            UpdateHealthUI(CurrentHealth);
+            CurrentHealth -= 10;
+            UpdateHealthUI(30);
 
         }
         if(Input.GetKeyDown("left")){
-            CurrentHealth -=10;
+            CurrentHealth +=10;
             UpdateHealthUI(CurrentHealth);
         }
     }
