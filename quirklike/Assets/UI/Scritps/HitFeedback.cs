43ebd78f619c84d18c3921217e678eccd1b5a5d5
Eddu <55552484+St4rman@users.noreply.github.com>
2024-10-28 14:46:33 +0000

    Hit feedback
    Added hit feedback for shotguns and default pistols. Bound to K key press right now, will rework this soon

---
+++ quirklike/Assets/UI/Scritps/HitFeedback.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HitFeedback : MonoBehaviour
+{
+    public Image[] HitFeedbackLines;
+
+    [SerializeField] private float FadeTime     = 0.2f;
+    [SerializeField] private Color DefaultColor = new Color(0,0,0,0);
+    [SerializeField] private Color HitColor     = new Color(0,0,0,1);
+    [SerializeField] private float cooldownTime = 0.1f;
+    private bool feedbackViz;
+    private float cooldown;
+
+    private void Awake()
+    {
+        foreach(Image H in HitFeedbackLines)
+        {
+            H.color = DefaultColor;
+        }
+        cooldown = cooldownTime;
+    }
+
+    private void ToggleHitFeedback(bool viz)
+    {
+        if(viz)
+        {
+            foreach(Image hit in HitFeedbackLines){
+                hit.DOColor(HitColor, FadeTime);
+            }
+        } else {
+            foreach(Image hit in HitFeedbackLines){
+                hit.DOColor(DefaultColor, FadeTime);
+            }
+        }
+    }
+    
+    public void SetHitFeedback(bool hit){
+        feedbackViz = hit;
+        ToggleHitFeedback(hit);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            SetHitFeedback(true);
+        }
+
+        if(feedbackViz)
+        {
+            cooldown -= Time.deltaTime;
+        }
+
+        if(cooldown< 0 && feedbackViz)
+        {
+            cooldown = cooldownTime;
+            feedbackViz = false;
+            ToggleHitFeedback(feedbackViz);
+        }
+    }
+}
