1f9f6fe881d5599b20de01784c2de415f3489c06
Eddu <55552484+St4rman@users.noreply.github.com>
2024-11-06 16:14:07 +0000

    bug fix
    Fixed a bug where dragging item on the same item slot

--- quirklike/Assets/UI/Scritps/itemSlot.cs
+++ quirklike/Assets/UI/Scritps/itemSlot.cs
@@ -7,8 +7,11 @@ public class itemSlot : MonoBehaviour, IDropHandler
 {
 
     public void OnDrop(PointerEventData eventData)
-    {
-
+    {   
+        if(transform.childCount != 0){
+            return;
+        }
+        
         GameObject dropped = eventData.pointerDrag;
         var draggableItem = dropped.GetComponent<UIGunIconDragable>();
 
